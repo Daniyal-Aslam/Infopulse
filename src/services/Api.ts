ea023daefd1214938a1e419928377a0a893d262d
Daniyal Aslam <daniyalaslam544@gmail.com>
2023-12-17 15:27:25 +0500

    Project Build

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,12 +1,11 @@
-import axios from 'axios';
-
+import axios, { AxiosResponse } from 'axios';
 const API_BASE_URL = 'https://newsapi.org/v2/everything';
 const API_KEY = 'efb0fc353b984f4192157054133f6131';  
 
 const Api = {
-  fetchData: async (category: string, language: string) => {
+  fetchData: async (category: string, language: string): Promise<AxiosResponse> => {
     try {
-      const response = await axios.get(API_BASE_URL, {
+      const response: AxiosResponse = await axios.get(API_BASE_URL, {
         params: {
           apiKey: API_KEY,
           q: category,
